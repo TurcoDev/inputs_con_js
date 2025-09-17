a23698fe3d44f35b0a22c7240fcfbdb4f70a8cc2
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2025-09-17 17:39:23 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+let colores = [];
+
+function cargarColor() {
+  const inputColor = document.getElementById('color');
+  colores.push(inputColor.value);
+  console.log(colores);
+}
+
+function mostrarColores(){
+  const contenedor = document.getElementById('contenedor');
+
+  contenedor.innerHTML = '';
+
+  for (let index = 0; index < colores.length; index++) {
+    contenedor.innerHTML += `<div class="caja-color" style="background-color: ${colores[index]}">${colores[index]}</div>`;
+  }
+
+  colores = [];
+  console.log(colores);
+  
+}
+
+
+function agregarTarjeta(){
+  const inputTitulo = document.getElementById('titulo');
+  const inputColor = document.getElementById('color2');
+  const inputParrafo = document.getElementById('parrafo');
+
+  const tarjetas = document.getElementById('tarjetas');
+
+  tarjetas.innerHTML += `
+    <div class="tarjeta">
+      <h3>${inputTitulo.value}</h3>
+      <div class="color-tarjeta" style="background-color: ${inputColor.value}">${inputColor.value}</div>
+      <p>${inputParrafo.value}</p>
+    </div>
+  `;
+}
